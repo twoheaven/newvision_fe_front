6e443bb328baa581541c5ca6be6f35007f367913
leecheonguk <dlcjsrnr0860@gmail.com>
2024-01-25 22:59:44 +0900

    footer 변경

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -8,13 +8,13 @@ import {
   Text,
 } from "@dohyun-ko/react-atoms";
 
-import colorSet from "@/styles/color-set";
+import logo from "../assets/logo.png"
 
 // 푸터를 나타내는 함수형 컴포넌트
 const Footer = () => {
   return (
     // 푸터 영역
-    <Area>
+    <Area backgroundColor="#494949">
       {/* 가로선 구분자 */}
       <Divider />
 
@@ -24,22 +24,26 @@ const Footer = () => {
       {/* 내용 섹션 - 회사 정보 및 소셜 미디어 링크 */}
       <Content>
         {/* 항목 간 간격을 두고 정렬하는 Flex 컨테이너 */}
-        <Flex justifyContent={"center"}>
+        <Flex justifyContent={"center"} gap={"50px"} alignItems="center">
           {/* 왼쪽 열 - 회사 정보 */}
-          <Flex
-            flexDirection={"column"}
-            style={{
-              color: colorSet.textGray,
-            }}
-          >
-            <Text size={"14px"}>
+          <Flex >
+            <img 
+              src={logo}
+              style={{
+                width: "182px",
+                height: "38px",
+              }}
+            />
+          </Flex>
+          <Flex flexDirection={"column"}>
+            <Text color={"white"} size={"14px"}>
               경기도 화성시 동탄문화센터로 7-3 (반송동), 거산프라자 10층 전관
             </Text>
-            <Text size={"14px"}>
+            <Text color={"white"} size={"14px"}>
               Tel. 031.613.0105 [Fax 겸용] / 010.4864.1433
             </Text>
             <Spacer height={"5px"} />
-            <Text size={"14px"}>
+            <Text color={"white"} size={"14px"}>
               Copyright (c) 2023 뉴비전교회 All rights reserved.
             </Text>
           </Flex>
