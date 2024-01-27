9053738ec6c0c75767c2627f1d7ff0d953239d9b
leecheonguk <dlcjsrnr0860@gmail.com>
2024-01-27 23:59:06 +0900

    인트로 글씨 빼고 다

--- src/pages/intro/IntroPage.tsx
+++ src/pages/intro/IntroPage.tsx
@@ -1,6 +1,13 @@
 import "./IntroPage.css";
 
-import { Area, Button, Content, Flex, Text } from "@dohyun-ko/react-atoms";
+import {
+  Area,
+  Button,
+  Content,
+  Flex,
+  Spacer,
+  Text,
+} from "@dohyun-ko/react-atoms";
 import { Link } from "react-router-dom";
 
 import Line from "@/components/line/Line";
@@ -27,23 +34,69 @@ const IntroPage = () => {
             NEWVISION
           </Text>
           <Line />
-          <Link to={Paths.Main}>
-            <Button
-              width={"300px"}
-              height={"300px"}
-              style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
-            >
-              <Flex
-                justifyContent="flex-end"
-                alignItems="flex-end"
-                flexDirection="column"
+          <Spacer height={"10px"} />
+          <Flex gap={"10px"}>
+            <Link to={Paths.Main}>
+              <Button
+                width={"300px"}
+                height={"300px"}
+                style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
               >
-                <Text color="white">뉴비전교회</Text>
-                <Text color="white">홈페이지</Text>
-                <Text color="white">바로가기</Text>
-              </Flex>
-            </Button>
-          </Link>
+                <Flex justifyContent="flex-end">
+                  <Text
+                    color="white"
+                    style={{
+                      position: "relative",
+                      top: "",
+                    }}
+                  >
+                    뉴비전교회 홈페이지 바로가기
+                  </Text>
+                </Flex>
+              </Button>
+            </Link>
+            <Flex flexDirection={"column"}>
+              <a href="https://www.youtube.com/@kimonyou153">
+                <Button
+                  width={"300px"}
+                  height={"145px"}
+                  style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+                >
+                  <Flex justifyContent="flex-end">
+                    <Text
+                      color="white"
+                      style={{
+                        position: "relative",
+                        top: "",
+                      }}
+                    >
+                      김온유 목사 유튜브 live 바로보기
+                    </Text>
+                  </Flex>
+                </Button>
+              </a>
+              <Spacer height={"10px"} />
+              <a href="https://www.youtube.com/watch?v=NlpUEUl1Zq8">
+                <Button
+                  width={"300px"}
+                  height={"145px"}
+                  style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+                >
+                  <Flex justifyContent="flex-end">
+                    <Text
+                      color="white"
+                      style={{
+                        position: "relative",
+                        top: "",
+                      }}
+                    >
+                      2023년 4기 요한계시록학교 신청하기
+                    </Text>
+                  </Flex>
+                </Button>
+              </a>
+            </Flex>
+          </Flex>
         </Flex>
       </Content>
     </Area>
