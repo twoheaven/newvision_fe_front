ffe4419379dcf3b61c5dc8282d92b3154ffc1b31
leecheonguk <dlcjsrnr0860@gmail.com>
2024-02-02 02:54:38 +0900

    다시

--- src/components/sermonCard/SermonCard.tsx
+++ src/components/sermonCard/SermonCard.tsx
@@ -3,7 +3,6 @@ import { Flex, Text } from "@dohyun-ko/react-atoms";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-// Importing a default image for cases where the video thumbnail is not available
 import defaultimg from "@/assets/default-image.svg";
 
 interface VideoData {
@@ -27,7 +26,13 @@ const SermonCard: React.FC<SermonCardProps> = ({
   height,
   width,
 }) => {
-  const [videoData, setVideoData] = useState<VideoData | null>(null);
+  const [videoData, setVideoData] = useState<VideoData | null>({
+    videoId,
+    thumbnailUrl: defaultimg,
+    publishedAt: "N/A",
+    playlistTitle: "N/A",
+    videoTitle: "N/A",
+  });
 
   useEffect(() => {
     const fetchVideoData = async () => {
@@ -35,25 +40,16 @@ const SermonCard: React.FC<SermonCardProps> = ({
         const response = await axios.get<VideoData>(
           `/api/youtubeData/${videoId}`,
         );
-        setVideoData(response.data);
+        setVideoData(response.data || videoData);
       } catch (error) {
         console.error("Error fetching video data", error);
-        // 데이터를 가져오지 못할 경우, 기본 데이터로 설정
-        setVideoData({
-          videoId,
-          thumbnailUrl: defaultimg, // 기본 이미지 주소
-          publishedAt: "N/A",
-          playlistTitle: "N/A",
-          videoTitle: "N/A",
-        });
       }
     };
 
     fetchVideoData();
-  }, [videoId]);
+  }, [videoId, videoData]);
 
   if (!videoData) {
-    // 데이터를 가져오지 못한 경우에도 기본 이미지를 보여줄 수 있도록 처리
     return (
       <Flex flexDirection="column" className="sermon-card">
         <img
--- src/pages/main/sermon/Sermon.tsx
+++ src/pages/main/sermon/Sermon.tsx
@@ -5,7 +5,6 @@ import { useEffect, useState } from "react";
 
 import SermonCard from "@/components/sermonCard/SermonCard";
 
-// 비디오 데이터의 형태를 정의합니다.
 interface VideoData {
   videoId: string;
   thumbnailUrl: string;
@@ -14,13 +13,10 @@ interface VideoData {
   videoTitle: string;
 }
 
-// Sermon 컴포넌트를 정의합니다.
-const Sermon = () => {
-  // 가장 최신 비디오 데이터를 저장할 상태를 선언합니다.
+const Sermon: React.FC = () => {
   const [latestVideoData, setLatestVideoData] = useState<VideoData[]>([]);
   const [cardWidth, setCardWidth] = useState(window.innerWidth / 6.5);
 
-  // 창 크기가 변할 때마다 cardWidth 업데이트
   useEffect(() => {
     const handleResize = () => {
       setCardWidth(window.innerWidth / 6.5);
@@ -33,28 +29,21 @@ const Sermon = () => {
     };
   }, []);
 
-  // 컴포넌트가 마운트될 때 최신 비디오 데이터를 가져오는 useEffect 훅을 정의합니다.
   useEffect(() => {
     const fetchLatestVideoData = async () => {
       try {
-        // 서버에서 최신 비디오 데이터를 가져오는 HTTP GET 요청
         const response = await axios.get<VideoData[]>("/api/latestYoutubeData");
-        // 받아온 데이터로 상태를 업데이트합니다.
-        setLatestVideoData(response.data.slice(0, 4)); // 배열의 길이를 최대 4개로 고정
+        setLatestVideoData(response.data.slice(0, 4));
       } catch (error) {
-        // 최신 비디오 데이터를 가져오는 도중에 발생하는 오류를 처리합니다.
         console.error("Error fetching latest video data", error);
       }
     };
 
-    // fetchLatestVideoData 함수를 호출합니다.
     fetchLatestVideoData();
   }, []);
 
-  // Sermon 컴포넌트를 렌더링합니다.
   return (
     <>
-      {/* 가장 최신 영상은 따로 표시합니다. */}
       <Flex gap={"25px"} flexDirection="column">
         <Flex gap={"25px"}>
           <div style={{ width: `${cardWidth * 1.3}px` }}>
@@ -79,7 +68,6 @@ const Sermon = () => {
                 videoId={latestVideoData[0].videoId || ""}
               />
             ) : (
-              // latestVideoData 배열이 비어있을 때에도 SermonCard를 렌더링합니다.
               <SermonCard
                 height={`${cardWidth * 0.6 * 1.3}px`}
                 width={`${cardWidth * 1.3}px`}
@@ -88,10 +76,8 @@ const Sermon = () => {
               />
             )}
           </Flex>
-          {/* 나머지 최신 영상은 Flex 컴포넌트를 사용하여 나열합니다. */}
           <Flex flexDirection="column">
             <Flex gap={"10px"}>
-              {/* latestVideoData가 비어있을 때에도 3번 반복하여 SermonCard를 렌더링합니다. */}
               {Array.from({ length: 3 }).map((_, index) => (
                 <SermonCard
                   key={index}
@@ -113,7 +99,6 @@ const Sermon = () => {
             <Text>성령학교 영상</Text>
             <Spacer height={"10px"} />
             <Flex gap={"10px"}>
-              {/* latestVideoData가 비어있을 때에도 3번 반복하여 SermonCard를 렌더링합니다. */}
               {Array.from({ length: 3 }).map((_, index) => (
                 <SermonCard
                   key={index}
@@ -134,11 +119,9 @@ const Sermon = () => {
           </Flex>
         </Flex>
       </Flex>
-      {/* 공간을 띄워주는 Spacer 컴포넌트를 추가합니다. */}
       <Spacer height={"50px"} />
     </>
   );
 };
 
-// Sermon 컴포넌트를 기본 내보내기로 내보냅니다.
 export default Sermon;
