fc405ae53309aa0a728cb9f987b4720e86c4f2f2
leecheonguk <dlcjsrnr0860@naver.com>
2024-04-03 00:00:31 +0900

    소개 목회사명

--- src/pages/introduce/introComponents/IntroCategory2.tsx
+++ src/pages/introduce/introComponents/IntroCategory2.tsx
@@ -1,12 +1,28 @@
-import { Text } from "@dohyun-ko/react-atoms";
+import { Content, Flex, Spacer, Text } from "@dohyun-ko/react-atoms";
+
+import useIsMobile from "@/hooks/useIsMobile";
+import Fonts from "@/styles/fonts";
+
+import missionimg from "./asset/목회사명.jpg";
 
 const IntroCategory2 = () => {
+  const isMobile = useIsMobile();
+
   return (
-    <>
-      <Text>IntroCategory2</Text>
-      <Text>IntroCategory2</Text>
-      <Text>IntroCategory2</Text>
-    </>
+    <Content>
+      <Flex justifyContent="center" alignItems="center" flexDirection="column">
+        <Text size={"24px"} font={Fonts.Bold}>
+          목회 사명
+        </Text>
+        <Spacer height={"5px"} />
+        <Text size={"13px"}>
+          뉴비전교회는 다음과 같은 목회 사명을 가지고 깨어 신부단장하는
+          교회입니다!
+        </Text>
+        <Spacer height={"50px"} />
+        <img src={missionimg} width={isMobile ? "80%" : "50%"}></img>
+      </Flex>
+    </Content>
   );
 };
 
