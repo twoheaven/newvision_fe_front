3bcc8df99b3efef881b54b1033e3ffeb897dcffc
leecheonguk <dlcjsrnr0860@naver.com>
2024-04-03 00:01:52 +0900

    아직 미완

--- src/pages/introduce/introComponents/IntroCategory6.tsx
+++ src/pages/introduce/introComponents/IntroCategory6.tsx
@@ -1,13 +1,88 @@
-import { Text } from "@dohyun-ko/react-atoms";
+import { Content, Flex, Spacer, Text } from "@dohyun-ko/react-atoms";
 
-const IntroCategory3 = () => {
+import useIsMobile from "@/hooks/useIsMobile";
+import Fonts from "@/styles/fonts";
+
+import visitimg from "./asset/교회직접방문.jpg";
+
+const IntroCategory6 = () => {
+  const isMobile = useIsMobile();
+
   return (
-    <>
-      <Text>IntroCategory3</Text>
-      <Text>IntroCategory3</Text>
-      <Text>IntroCategory3</Text>
-    </>
+    <Content>
+      <Flex justifyContent="center" alignItems="center" flexDirection="column">
+        <Text size={"24px"} font={Fonts.Bold}>
+          새가족 안내
+        </Text>
+        <Spacer height={"5px"} />
+        <Text size={"13px"}>
+          교회에 처음 오신 새가족 분들께 교회안내와 함께 등록 절차를
+          안내드립니다.
+        </Text>
+        <Spacer height={"50px"} />
+      </Flex>
+      <Flex gap={"5px"} style={{ marginLeft: "50px" }} alignItems="center">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignContent: "center",
+            width: "24px",
+            height: "24px",
+            padding: "0.3em",
+            borderRadius: "50%",
+            backgroundColor: "#7DAFE2",
+            color: "white",
+          }}
+        >
+          <Text
+            size={"28px"}
+            font={Fonts.Bold}
+            color="white"
+            style={{
+              transform: "translate(-1px,-9.5px)",
+            }}
+          >
+            1
+          </Text>
+        </div>
+        <Text size={"20px"} font={Fonts.Bold}>
+          교회 직접 방문
+        </Text>
+        <Text>정식 등록</Text>
+      </Flex>
+      <Spacer height={"10px"} />
+      <Flex justifyContent="center" alignItems="center" flexDirection="column">
+        <img src={visitimg} width={isMobile ? "90%" : "70%"}></img>
+        <Spacer height={"20px"} />
+      </Flex>
+      <Flex style={{ marginLeft: "50px" }}>
+        <Flex gap={"5px"}>
+          <Text font={Fonts.Bold}>1. 교회등록</Text>
+          <Text>
+            본당 내에 있는 '새가족 등록카드'를 작성하시고 제출해주십시오.
+          </Text>
+        </Flex>
+        <Flex gap={"5px"}>
+          <Text font={Fonts.Bold}>2. 등록기간</Text>
+          <Text>
+            등록기간 동안 유튜브를 통하여 말씀을 듣고 준비하는 기간을 가집니다.
+          </Text>
+        </Flex>
+        <Flex gap={"5px"}>
+          <Text font={Fonts.Bold}>3. 담임 목사님과 만남</Text>
+          <Text>등록기간이 지나면, 담임목사님과 상담 및 인사가 있습니다.</Text>
+        </Flex>
+        <Flex gap={"5px"}>
+          <Text font={Fonts.Bold}>4. 등록 심방</Text>
+          <Text>
+            담임목사님께서 심방을 원하는 가정이 편한 시간과 장소에서 심방을 할
+            수 있습니다.
+          </Text>
+        </Flex>
+      </Flex>
+    </Content>
   );
 };
 
-export default IntroCategory3;
+export default IntroCategory6;
--- src/pages/introduce/introComponents/IntroCategory7.tsx
+++ src/pages/introduce/introComponents/IntroCategory7.tsx
@@ -1,13 +1,26 @@
-import { Text } from "@dohyun-ko/react-atoms";
+import { Content, Flex, Spacer, Text } from "@dohyun-ko/react-atoms";
 
-const IntroCategory3 = () => {
+import Fonts from "@/styles/fonts";
+
+import missionimg from "./asset/목회사명.jpg";
+
+const IntroCategory7= () => {
   return (
-    <>
-      <Text>IntroCategory3</Text>
-      <Text>IntroCategory3</Text>
-      <Text>IntroCategory3</Text>
-    </>
+    <Content>
+      <Flex justifyContent="center" alignItems="center" flexDirection="column">
+        <Text size={"24px"} font={Fonts.Bold}>
+          목회 사명
+        </Text>
+        <Spacer height={"5px"} />
+        <Text size={"13px"}>
+          뉴비전교회는 다음과 같은 목회 사명을 가지고 깨어 신부단장하는
+          교회입니다!
+        </Text>
+        <Spacer height={"50px"} />
+        <img src={missionimg} width={"50%"}></img>
+      </Flex>
+    </Content>
   );
 };
 
-export default IntroCategory3;
+export default IntroCategory7;
