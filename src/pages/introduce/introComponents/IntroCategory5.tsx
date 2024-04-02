583b25b3c67f4783613275a0e141a9d282a12f06
leecheonguk <dlcjsrnr0860@naver.com>
2024-04-03 00:01:38 +0900

    소개 오시는 길

--- src/pages/introduce/introComponents/IntroCategory5.tsx
+++ src/pages/introduce/introComponents/IntroCategory5.tsx
@@ -1,13 +1,25 @@
-import { Text } from "@dohyun-ko/react-atoms";
+import { Content, Flex, Spacer, Text } from "@dohyun-ko/react-atoms";
 
-const IntroCategory2 = () => {
+import useIsMobile from "@/hooks/useIsMobile";
+import Fonts from "@/styles/fonts";
+
+import roadimg from "./asset/오시는길.jpg";
+
+const IntroCategory5 = () => {
+  const isMobile = useIsMobile();
   return (
-    <>
-      <Text>IntroCategory2</Text>
-      <Text>IntroCategory2</Text>
-      <Text>IntroCategory2</Text>
-    </>
+    <Content>
+      <Flex justifyContent="center" alignItems="center" flexDirection="column">
+        <Text size={"24px"} font={Fonts.Bold}>
+          오시는 길
+        </Text>
+        <Spacer height={"5px"} />
+        <Text size={"13px"}>뉴비전교회 오시는 길을 안내합니다.</Text>
+        <Spacer height={"50px"} />
+        <img src={roadimg} width={isMobile ? "85%" : "50%"}></img>
+      </Flex>
+    </Content>
   );
 };
 
-export default IntroCategory2;
+export default IntroCategory5;
