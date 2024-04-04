8f9fa283daf5e9c6bb028d7840f231a73cb1e614
leecheonguk <dlcjsrnr0860@naver.com>
2024-04-04 13:11:12 +0900

    배너 조절!

--- src/pages/main/newbanner/NewBannerSection.tsx
+++ src/pages/main/newbanner/NewBannerSection.tsx
@@ -1,26 +1,36 @@
 import { Flex, Spacer } from "@dohyun-ko/react-atoms";
 import React from "react";
 
+import useIsMobile from "@/hooks/useIsMobile";
 import Header2 from "@/layout/Header2";
 
 import mainImg from "./assets/mainpageimg.png";
 
 const NewBannerSection = () => {
+  const isMobile = useIsMobile();
   return (
     <Flex justifyContent="center" width="100%">
       <Flex width="100%" style={{ position: "relative", textAlign: "center" }}>
         {/* 배너 이미지 표시 */}
-        <img
-          width="100%"
-          src={mainImg}
-          alt="mainimg"
-          style={{
-            pointerEvents: "none",
-            width: "100%",
-            minWidth: "100%",
-          }}
-        />
-
+        <div style={{ textAlign: "center" }}>
+          <img
+            width={isMobile ? "150%" : "100%"}
+            src={mainImg}
+            alt="mainimg"
+            style={
+              isMobile
+                ? {
+                    pointerEvents: "none",
+                    display: "block",
+                    transform: "translateX(-10%)",
+                  }
+                : {
+                    pointerEvents: "none",
+                    display: "block",
+                  }
+            }
+          />
+        </div>
         {/* 헤더 */}
         <Flex
           justifyContent="center"
