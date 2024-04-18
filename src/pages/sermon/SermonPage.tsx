41bdd879040fab9645c3a5e2824e8367a9073809
leecheonguk <dlcjsrnr0860@naver.com>
2024-04-18 14:29:52 +0900

    설교

--- src/pages/sermon/SermonPage.tsx
+++ src/pages/sermon/SermonPage.tsx
@@ -1,15 +1,25 @@
 import { Area, Content, Spacer } from "@dohyun-ko/react-atoms";
+import { Helmet } from "react-helmet-async";
 
 import CelebrityPageHeader from "@/components/pageHeader/SermonPageHeader";
 
 const SermonPage = () => {
   return (
-    <Area>
-      <CelebrityPageHeader />
-      <Spacer height={"30px"} />
-      <Content></Content>
-      <Spacer height={"100px"} />
-    </Area>
+    <>
+      <div>
+        <Helmet>
+          <title>김온유 목사 설교 말씀</title>
+          <meta name="descripttion" content="김온유 목사 설교 말씀" />
+        </Helmet>
+      </div>
+
+      <Area>
+        <CelebrityPageHeader />
+        <Spacer height={"30px"} />
+        <Content></Content>
+        <Spacer height={"100px"} />
+      </Area>
+    </>
   );
 };
 
